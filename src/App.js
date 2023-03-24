2bfa18adb6da2edc0898fa2bcb4290385d51d24e
prabakaran <mspk028@gmail.com>
2023-03-24 15:43:22 +0530

    header completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import { Header } from "./components";
 import "./styles/App.css";
 
@@ -6,8 +7,15 @@ import "./styles/App.css";
 
 const App = () => {
   return (
-    <div className="w-screen h-auto flex flex-col">
+    <div className="w-screen h-auto flex flex-col bg-primary">
       <Header />
+
+      <main className="mt-24 p-8 w-full">
+        <Routes>
+          <Route path="/" element={<MainContainer />} />
+          <Route path="/" element={<CreateContainer />} />
+        </Routes>
+      </main>
     </div>
   );
 };

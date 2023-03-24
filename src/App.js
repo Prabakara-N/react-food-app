8a457e320e70b98d317f36e70c2b771e2c94bae0
prabakaran <mspk028@gmail.com>
2023-03-24 14:58:27 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header } from "./components";
+import "./styles/App.css";
+
+// import { FaEdit } from "react-icons/fa";
+
+const App = () => {
+  return (
+    <div className="w-screen h-auto flex flex-col">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
